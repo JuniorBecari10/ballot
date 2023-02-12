4b580fb6d3d66508c5ebacba9ab9bf93796d9f41
Antônio Carlos <juniorbecari@gmail.com>
2023-02-12 16:56:29 -0300

    Added first option menu and added .gitignore

--- main.go
+++ main.go
@@ -11,8 +11,29 @@ func main() {
   
   for {
     Clear()
-    fmt.Println("Ballot Box Creator")
+    fmt.Println("Ballot Box Creator\n")
     
+    fmt.Println("Choose an option:\n")
+    
+    fmt.Println("1 - Create new ballot box")
+    fmt.Println("2 - Open an existing ballot box")
+    
+    fmt.Println("0 - Exit")
+    
+    fmt.Print("\n> ")
     sc.Scan()
+    op := sc.Text()
+    
+    switch op {
+      case "1":
+        break
+      
+      case "2":
+        break
+      
+      case "0":
+        Clear()
+        os.Exit(0)
+    }
   }
 }
