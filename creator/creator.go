cc6910d499f66f5af692ad0cd64a66f303455a52
Antônio Carlos <juniorbecari@gmail.com>
2023-02-13 14:36:30 -0300

    Now you can add sections

--- creator/creator.go
+++ creator/creator.go
@@ -4,18 +4,28 @@ import (
   "fmt"
   
   "ballot/util"
+  "strconv"
 )
 
 var editing *Ballot
 
 type Ballot struct {
   Name string
-  Sections []Section  
+  Sections []*Section  
+}
+
+func NewBallot(name string) *Ballot {
+  return &Ballot { Name: name }
 }
 
 type Section struct {
+  Name string
   Candidates []Candidate
   NumberLength int
+}
+
+func NewSection(name string, numberLen int) *Section {
+  return &Section { Name: name, NumberLength: numberLen }
 }
 
 type Candidate struct {
@@ -23,10 +33,6 @@ type Candidate struct {
   Vice string
   
   Number string // yes, a string. Because you can set "05" as a number.
-}
-
-func NewBallot(name string) *Ballot {
-  return &Ballot { Name: name }
 }
 
 func CreateMenu() {
@@ -55,11 +61,53 @@ func MainMenu() {
     
     switch op {
       case "1":
-        
+        EditSections()
         break
       
       case "2":
         EditBallotName()
+        break
+      
+      case "0":
+        return
+    }
+  }
+}
+
+func EditSections() {
+  for {
+    util.Clear()
+    util.PrintName()
+    fmt.Printf("Editing ballot %s / sections\n", editing.Name)
+    
+    fmt.Println("\nSections:\n")
+    
+    if len(editing.Sections) > 0 {
+      for _, s := range editing.Sections {
+       fmt.Printf("%s | %d candidates | %d numbers\n", s.Name, len(s.Candidates), s.NumberLength); 
+      }
+    } else {
+      fmt.Println("There are no sections.")
+    }
+    
+    fmt.Println("\nChoose an option:\n")
+    
+    fmt.Println("1 - Add new section")
+    fmt.Println("2 - Edit an existing section")
+    
+    fmt.Println("0 - Go back")
+    
+    fmt.Print("\n> ")
+    util.Scanner.Scan()
+    op := util.Scanner.Text()
+    
+    switch op {
+      case "1":
+        AddSection()
+        break
+      
+      case "2":
+        
         break
       
       case "0":
@@ -73,4 +121,24 @@ func EditBallotName() {
   util.Scanner.Scan()
   
   editing.Name = util.Scanner.Text()
+}
+
+// ----
+
+func AddSection() {
+  fmt.Print("Enter the section name: ")
+  util.Scanner.Scan()
+  name := util.Scanner.Text()
+  
+  fmt.Print("Enter the number length: ")
+  util.Scanner.Scan()
+  lenStr := util.Scanner.Text()
+  
+  len, err := strconv.Atoi(lenStr)
+  
+  if err != nil {
+    return
+  }
+  
+  editing.Sections = append(editing.Sections, NewSection(name, len))
 }
